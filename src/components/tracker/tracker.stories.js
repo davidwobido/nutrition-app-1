68bd5486ac546205b0111b20ef6e14c283c7d8d6
davidwobido <davidwobido@googlemail.com>
2021-10-08 11:43:28 +0200

    Add Purple Button

---
+++ src/components/tracker/tracker.stories.js
@@ -0,0 +1,9 @@
+import "./tracker.css";
+import tracker from "./tracker.html";
+
+export default {
+  title: "Components/tracker",
+  parameters: { layout: "centered" },
+};
+
+export const headers = () => tracker;
