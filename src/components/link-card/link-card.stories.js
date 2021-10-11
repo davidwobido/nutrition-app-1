163c124b9956b45ccdae4b85fcbb7959ffd6c51b
davidwobido <davidwobido@googlemail.com>
2021-10-11 12:38:58 +0200

    Change Tracker to Link Card

---
+++ src/components/link-card/link-card.stories.js
@@ -0,0 +1,19 @@
+import "./link-card.css";
+import linkcard from "./link-card.html";
+
+export default {
+  title: "Components/link-card",
+  parameters: { layout: "centered" },
+};
+
+export const headers = () => linkcard;
+
+// import "./tracker.css";
+// import tracker from "./tracker.html";
+
+// export default {
+//   title: "Components/tracker",
+//   parameters: { layout: "centered" },
+// };
+
+// export const headers = () => tracker;
--- src/components/tracker/tracker.stories.js
+++
@@ -1,9 +0,0 @@
-import "./tracker.css";
-import tracker from "./tracker.html";
-
-export default {
-  title: "Components/tracker",
-  parameters: { layout: "centered" },
-};
-
-export const headers = () => tracker;
